bf1664d165c036208264825f1903a94324f9784f
LuisMartinez1 <34143705+LuisMartinez1@users.noreply.github.com>
2023-09-07 19:07:23 -0600

    Adding project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # TVCartoons
-A project where you can see cartoons from the 80's, 90's and early 2000's in Latin Spanish.
+
+This is a project where you can watch cartoons from the 80s, 90s, and 2000s, based on the website LACartoons (you can visit it [here](https://www.lacartoons.com/)). We've created a simple random video player with the design of an old television, complete with a tuner reminiscent of those years.
+
+## Demo: [TVCartoons](http://tvcartoons.somee.com/)
+
+When you enter the site, you'll be greeted by a TV screen. You can watch it on your mobile in portrait mode, on an iPad, or on a PC without any issues; however, there is a minor CSS bug. You can change the channels, but please note that it might take a moment to find a playable episode, as some of them have been removed. The player will continue looping until it finds a playable episode.
+
+Enjoy your nostalgic trip down memory lane!
---
+++ TVCartoons.Web/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TVCartoons.Web.Models;
+
+namespace TVCartoons.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUrl()
+        {
+            return Json(await GenerateUrl());
+        }
+
+        public async Task<string> GenerateUrl()
+        {
+            var random = new Random();
+            var success = false;
+            var urlSucces = string.Empty;
+            do
+            {
+                try
+                {
+                    var url = $"https://lacartoons.com/serie/capitulo/{random.Next(1, 31001)}";
+                    using (var httpClient = new HttpClient())
+                    {
+                        var html = await httpClient.GetStringAsync(url);
+                        var doc = new HtmlDocument();
+                        doc.LoadHtml(html);
+                        var iframeNode = doc.DocumentNode.SelectSingleNode("//iframe");
+                        if (iframeNode != null)
+                        {
+                            var iframeSrc = iframeNode.GetAttributeValue("src", "");
+                            var videoHtml = await httpClient.GetStringAsync(iframeSrc);
+                            success = true;
+                            urlSucces = iframeSrc;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Error: {ex.InnerException?.Message ?? ex.Message}");
+                }
+            } while (!success);
+            return urlSucces;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
